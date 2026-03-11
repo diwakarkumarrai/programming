616eb97e12f9c17587152fb11d4fcba0885082dd
Diwakar kumar <bhaidiwakar30@gmail.com>
2026-03-11 18:45:46 +0530

    writing code to check array is sorted or not in two ways efficient way and not efficient way

--- src/java_array_question.java
+++ src/java_array_question.java
@@ -17,6 +17,7 @@ public class java_array_question {
         */
 
 
+        /*
         //WAP to find the given element is present is array or not
         Scanner sc = new Scanner(System.in);
 
@@ -41,7 +42,108 @@ public class java_array_question {
         } else {
             System.out.println("The target element is not present in this array ");
         }
-    }
 
+
+         */
+
+        /*
+        //WAP to find array is sorted or not
+        Scanner sc = new Scanner(System.in);
+
+        float[] arr = new float[5];
+        boolean count1 = false;
+        boolean count2 = false;
+        boolean count3 = false;
+
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter the " + (i + 1) + " element for this array ");
+            arr[i] = sc.nextFloat();
+        }
+
+        for(int j = 0; j<arr.length-1;j++){
+            if(arr[j]<arr[j+1]){
+                System.out.println("This array is not sorted in descending order");
+                count1=false;
+               break;
+
+            }
+            else{
+                count1=true;
+            }
+
+
+        }
+
+        for(int j = 0; j<arr.length-1;j++){
+            if(arr[j]>arr[j+1]){
+                System.out.println("This array is not sorted in Ascending order");
+                count2=false;
+               break;
+            }
+            else{
+                count2=true;
+            }
+
+        }
+        for(int j = 0; j<arr.length-1;j++){
+            if(arr[j]==arr[j+1]){
+                count3=true;
+            }
+            else{
+                count3=false;
+                break;
+            }
+        }
+
+
+        if(count3){
+            System.out.println("The all entered element in this array is equal");
+        }
+        else if(count1){
+            System.out.println("This array is sorted in Descending order");
+
+        }
+        else if(count2){
+            System.out.println("This array is sorted in Ascending order");
+        }
+
+
+         */
+
+
+        /*
+        // efficient way to check array is sorted or not
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter the element " + (i + 1) + " for array ");
+            arr[i] = sc.nextInt();
+        }
+        boolean ascending = true;
+        boolean descending = true;
+
+        for (int j = 0; j < arr.length - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                descending = false;
+            }
+            if (arr[j] > arr[j + 1]) {
+                ascending = false;
+            }
+        }
+
+        if(ascending){
+            System.out.println("Array is sorted in Ascending order");
+        }
+        else if(descending){
+            System.out.println("Array is sorted in descending order");
+        }
+        else{
+            System.out.println("Array is not sorted");
+        }
+
+         */
+
+    }
 }
 
