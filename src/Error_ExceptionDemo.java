c2d84dbc27ecd03fb85f23521063a1a6f8c63e5d
Diwakar kumar <bhaidiwakar30@gmail.com>
2026-05-05 18:40:48 +0530

    handling exception using try and catch block

--- src/Error_ExceptionDemo.java
+++ src/Error_ExceptionDemo.java
@@ -1,5 +1,5 @@
 //import java.util.Scanner;
-
+/*
 public class Error_ExceptionDemo {
     public static void main(String[] args) {
         // SYNTAX ERROR
@@ -20,5 +20,32 @@ public class Error_ExceptionDemo {
         System.out.println("The answer when we divide 1000 by "+k+" is " + 1000/k);
         //this will throw error when we input anything else at place of integer
 
+    }
+}
+
+
+ */
+
+
+//using try and catch block to handle exception
+public class Error_ExceptionDemo{
+    public static void main(String[] args) {
+       int a = 1000;
+       int b = 0;
+
+       //without try - catch block --> this will throw error and program execution will stop here.
+          // int c =a/b;
+          // System.out.println("The result of division of a/b is "+c);
+
+       //with try - catch block
+       try{
+           int c = a/b;
+           System.out.println("The result of a/b is "+c);
+       } catch (Exception e) {
+           System.out.println("This code is not running. Reason is: ");
+           System.out.println(e);
+       }
+
+        System.out.println("End of the program");
     }
 }
