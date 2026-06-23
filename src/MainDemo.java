04f54cd4c552c6d7f6843265f0b988ac5d38ee72
Diwakar kumar <bhaidiwakar30@gmail.com>
2026-06-23 23:57:59 +0530

    various way to declare a main method

--- src/MainDemo.java
+++ src/MainDemo.java
@@ -1,5 +1,70 @@
+/*
 public class MainDemo {
     public static void main(String[] args) {
 
     }
+}
+
+ */
+
+// we can also write main method like this
+/*
+public class MainDemo {
+    public static void main(String... args) { //String... args means String[] args
+
+    }
+}
+ */
+
+// we can also write main method like this
+/*
+public class MainDemo {
+    public static void main(String[] diwakar) {
+
+    }
+}
+
+ */
+// we can also write main method like this
+/*
+public class MainDemo {
+    public static void main(String  [] args) {
+
+    }
+}
+
+ */
+
+// we can also write main method like this
+/*
+public class MainDemo {
+     static public  void main(String[] args) {
+
+    }
+}
+ */
+
+import com.sun.source.tree.SynchronizedTree;
+
+// we can also write main method like this
+/*
+public class MainDemo {
+    public  synchronized strictfp  static void main(String[] args) {
+
+    }
+}
+
+ */
+
+// we can also write main method like this
+
+public class MainDemo {
+    static {
+        System.out.println("This is static block");
+
+    }
+    public static void main(String[] args){
+        System.out.println("This is main method");
+    }
+
 }
