aca3c6126d8f3ba652916a5ccdfcb10d47a7ff7b
Diwakar kumar <bhaidiwakar30@gmail.com>
2026-06-25 23:55:21 +0530

    code

--- src/OperatorDemo.java
+++ src/OperatorDemo.java
@@ -1,5 +1,12 @@
 public class OperatorDemo {
     public static void main(String... args) {
 
+        byte a = 10;
+        //a= a+10; // not valid because by default java considered a and 10 as int then perform addition then the result is of int datatype ,and we can fit int into byte
+        a = (byte)(a+10); // we have to typecast in byte
+        System.out.println(a);
+       // System.out.println(10 / 0);//this will give runtime exception : / by zero
+        System.out.println(10/0.0);//this will give output as : infinity
+
     }
 }
