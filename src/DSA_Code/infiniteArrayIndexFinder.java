07f113ad654b965714386006aa4e32b5fa9776e4
Diwakar kumar <bhaidiwakar30@gmail.com>
2026-07-18 18:01:29 +0530

    code

---
+++ src/DSA_Code/infiniteArrayIndexFinder.java
@@ -0,0 +1,28 @@
+package DSA_Code;
+// Code to search the index of a given element in an infinite array
+public class infiniteArrayIndexFinder {
+    static public  int searchTarget(int [] arr , int target ) {
+        int start =0;
+        int end = 1;
+        while(target>arr[end]){
+            int newStart = end+1;
+            end =end+(end-start+1)* 2 ;
+            start =newStart;
+        }
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]==target) return mid;
+            else if (target > arr[mid]) start = mid+1;
+            else end = mid-1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[1000];
+        for (int i = 0; i < 1000; i++) {
+            arr[i] = i + 1;  // values from 1 to 50
+        }
+        System.out.println(searchTarget(arr,510));
+    }
+}
